29463264e2ff7a6386432be6ec40d10b2fd09d6a
ONIGOROSHI <onigoro3@gmail.com>
2025-12-04 05:33:25 +0900

    aa

--- app/pdf/page.tsx
+++ app/pdf/page.tsx
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import Link from 'next/link';
 // ★ PDF表示用のライブラリをインポート
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 // ★ PDFエンジンのワーカー設定（これがないと動きません）
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
